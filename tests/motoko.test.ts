1da1375cd6af1fdd7b3f6adda7b1179b227f8829
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-08-23 18:58:15 -0600

    Rollback from 'func()' to 'func ()'

--- tests/motoko.test.ts
+++ tests/motoko.test.ts
@@ -55,7 +55,7 @@ describe('Motoko formatter', () => {
     });
 
     test('anonymous functions', () => {
-        expect(format('func():() {}')).toStrictEqual('func() : () {}\n');
+        expect(format('func():() {}')).toStrictEqual('func () : () {}\n');
     });
 
     test('multi-line statement indentation', () => {
