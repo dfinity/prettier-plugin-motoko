3b59e179aae2221e5d4fee1af0c84b3c1a5b691a
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-08-25 16:29:32 -0600

    Fix top-level module exports

---
+++ src/defaultOptions.ts
@@ -0,0 +1,3 @@
+const defaultOptions = {};
+
+export default defaultOptions;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-import { Plugin } from 'prettier';
 import languages from './languages';
 import parsers from './parsers';
 import printers from './printers';
 import options from './options';
+import defaultOptions from './defaultOptions';
 
-const plugin: Plugin = {
-    languages,
-    parsers,
-    printers,
-    options,
-};
-
-export default plugin;
+export { languages, parsers, printers, options, defaultOptions };
