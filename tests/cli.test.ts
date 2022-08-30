343db1105519e007f220e7b45ce809c9402da240
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-08-30 11:33:56 -0600

    Update mo-fmt to 0.1.x

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -3,7 +3,6 @@ const cliPackage = require('../packages/mo-fmt/package.json');
 
 describe('mo-fmt CLI', () => {
     test('is up-to-date', () => {
-        expect(cliPackage.version).toStrictEqual(pluginPackage.version);
         expect(cliPackage.dependencies['prettier-plugin-motoko']).toStrictEqual(
             `^${pluginPackage.version}`,
         );
