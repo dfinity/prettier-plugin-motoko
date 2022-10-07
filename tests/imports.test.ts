ab92c7ec190e50ee9c07f0bdf172f1e106adeb3a
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-10-07 10:29:03 -0600

    Merge

--- tests/imports.test.ts
+++ tests/imports.test.ts
@@ -10,15 +10,37 @@ const prettierOptions: prettier.Options = {
 };
 
 const format = (input: string, options?: prettier.Options): string => {
+    console.log(prettier.format(input, { ...prettierOptions, ...options })); //////
     return prettier.format(input, { ...prettierOptions, ...options });
 };
 
 describe('Motoko imports', () => {
+    test('basic imports', () => {
+        expect(format('import a "a"')).toStrictEqual('import a "a"');
+        expect(format('import a "a";\n\na')).toStrictEqual(
+            'import a "a";\n\na;\n',
+        );
+    });
+
+    test('object pattern imports', () => {
+        expect(format('import {x} "a"')).toStrictEqual('import { x } "a";');
+        expect(format('import {x\n;y} "a"')).toStrictEqual(
+            'import {\n  x;\n  y;\n} "a";',
+        );
+        expect(format('import {x; y} "b"; import {x\n;y} "a"')).toStrictEqual(
+            'import {\n  x;\n  y;\n} "a";\nimport {\n  x;\n  y;\n} "b";',
+        );
+    });
+
     test('empty line after imports', () => {
-        expect(format('import a "a"; a')).toStrictEqual('import a "a";\n\na;\n');
+        expect(format('import a "a"; a')).toStrictEqual(
+            'import a "a";\n\na;\n',
+        );
     });
 
     test('combine imports', () => {
-        expect(format('import b "b";\n// xyz\nimport b "b"')).toStrictEqual('import a "a";\nimport b "b";\n\n// xyz\n');
+        expect(format('import b "b";\n// xyz\nimport b "b"')).toStrictEqual(
+            'import a "a";\nimport b "b";\n\n// xyz\n',
+        );
     });
 });
