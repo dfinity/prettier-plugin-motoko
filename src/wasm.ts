d2a7e0684d33d15decc2e3f42def6c00906d7743
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-10-15 12:56:13 -0600

    Remove commented code

--- src/wasm.ts
+++ src/wasm.ts
@@ -1,19 +1,3 @@
-// const wasm =
-//     typeof window === 'undefined'
-//         ? require(String('../wasm/pkg/nodejs/wasm')) // Guard against bundling both wasm files
-//         : require('../wasm/pkg/bundler/wasm');
-
-// const wasm =
-//     typeof window === 'undefined'
-//         ? require(String('../wasm/pkg/nodejs/wasm')) // Guard against bundling both wasm files
-//         : require('../wasm/pkg/bundler/wasm');
-
-// export default wasm;
-
-// import * as wasm from '../wasm/pkg/bundler/wasm';
-
-// export default wasm;
-
 let wasm: any = {};
 
 export const setWasm = (newWasm: object) => {
