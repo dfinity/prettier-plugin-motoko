0c646e7297ec89ab1f4ecf63376d3250a7fd671d
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-12-19 14:32:23 -0700

    Fix obscure lexer corner case (#75)
    * Fix obscure lexer corner case
    
    * Test keep-space for obscure corner case
    
    * 0.2.5
    
    * Bump mo-fmt dependency

--- tests/formatter.test.ts
+++ tests/formatter.test.ts
@@ -247,6 +247,11 @@ describe('Motoko formatter', () => {
     //     );
     // });
 
+    test('identifier tokens', ()=>{
+        expect(format('x.0.e0x')).toStrictEqual('x.0.e0x\n');
+        expect(format('x.0.e0 x')).toStrictEqual('x.0.e0 x\n');
+    })
+
     test('array indexing line break', () => {
         expect(format(`${'x'.repeat(80)}[0]`)).toStrictEqual(
             `${'x'.repeat(80)}[0];\n`,
