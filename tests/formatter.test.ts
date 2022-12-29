884984e5c83afbeaa26c3f382bc1cd8549c5f307
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-12-29 14:12:04 -0700

    Automatically insert semicolons after multi-line blocks (#78)
    * Automatically insert semicolons after multi-line blocks, configurable via 'semi' config
    
    * 0.3.0
    
    * Add test cases for automatic semicolons
    
    * Update mo-fmt
    
    * Run 'npm audit fix'

--- tests/formatter.test.ts
+++ tests/formatter.test.ts
@@ -38,7 +38,7 @@ describe('Motoko formatter', () => {
 
     test('line comments', () => {
         expect(format('{//\n}//')).toStrictEqual('{\n  //\n} //\n');
-        expect(format('{\n//\n}\n//')).toStrictEqual('{\n  //\n}\n//\n');
+        expect(format('{\n//\n}\n//')).toStrictEqual('{\n  //\n};\n//\n');
         expect(format('//a\n//b')).toStrictEqual('//a\n//b\n');
         expect(format('//a\n\n\n//b')).toStrictEqual('//a\n\n//b\n');
     });
@@ -315,6 +315,11 @@ describe('Motoko formatter', () => {
         expect(format('x.0.y')).toStrictEqual('x.0.y\n');
         expect(format('0. y')).toStrictEqual('0. y\n');
         expect(format('0.\ny')).toStrictEqual('0.\ny;\n');
+    });
+
+    test('automatic semicolons', () => {
+        expect(format('{\n}\nA\n')).toStrictEqual('{};\nA;\n');
+        expect(format('{\n// }\n}\nA\n')).toStrictEqual('{\n  // }\n};\nA;\n');
     });
 
     // test('generate diff files from compiler tests', () => {
