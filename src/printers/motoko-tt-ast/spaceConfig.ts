f6c342f8d95c5fc7011b0ecd64fb87c524637c36
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2023-04-17 13:10:45 -0600

    Allow multi-line text concatenation (#96)
    * Keep multi-line text concatenation
    
    * 0.5.0
    
    * Remove commented code
    
    * Bump mo-fmt

--- src/printers/motoko-tt-ast/spaceConfig.ts
+++ src/printers/motoko-tt-ast/spaceConfig.ts
@@ -128,10 +128,14 @@ const spaceConfig: SpaceConfig = {
         [{ left: tokenEquals('if'), main: '_' }, 'Paren', 'space'],
 
         // unary operators
-        [tokenEquals('#'), 'Ident', 'keep'],
         [tokenEquals('+'), '_', 'keep'],
         [tokenEquals('-'), '_', 'keep'],
         [tokenEquals('^'), '_', 'keep'],
+        // [tokenEquals('#'), 'Ident', 'keep'],
+
+        // tags and concatenation
+        [tokenEquals('#'), '_', 'keep'],
+        ['_', tokenEquals('#'), 'keep'],
 
         // 'with' keyword
         [tokenEquals('with'), '_', 'keep-space'],
@@ -146,7 +150,6 @@ const spaceConfig: SpaceConfig = {
         [{ left: tokenEquals('do'), main: tokenEquals('?') }, '_', 'space'],
         // [tokenEquals('?'), 'Curly', 'keep'],
         [tokenEquals('?'), '_', 'nil'],
-        // [tokenEquals('#'), 'Ident', 'nil'], ///
         ['_', tokenEquals('!'), 'nil'],
 
         // space between identifier and group
