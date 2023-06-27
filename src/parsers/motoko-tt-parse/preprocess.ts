e7616fdf3e0912fc476fdc68ed5b750eca64a4b1
rvanasa <ryan.vandersmith@dfinity.org>
2023-06-27 13:15:36 -0600

    Deactivate end-of-block semicolon insertion (relied on single-line text literals)

--- src/parsers/motoko-tt-parse/preprocess.ts
+++ src/parsers/motoko-tt-parse/preprocess.ts
@@ -9,37 +9,37 @@ export default function preprocess(
     code = code.replace(/[ \t]+(?=\r?\n)/g, ''); // remove trailing spaces
     code = outOfCharacter.replace(code); // remove invisible unicode characters
 
-    if (options.semi) {
-        // Infer semicolons
-        let nextIndent = 0;
-        const reversedLines = code.split('\n').reverse();
-        code = reversedLines
-            .map((line: string, i: number) => {
-                const trimmedLine = line.trim();
-                if (!trimmedLine) {
-                    return line;
-                }
+    // if (options.semi) {
+    //     // Infer semicolons
+    //     let nextIndent = 0;
+    //     const reversedLines = code.split('\n').reverse();
+    //     code = reversedLines
+    //         .map((line: string, i: number) => {
+    //             const trimmedLine = line.trim();
+    //             if (!trimmedLine) {
+    //                 return line;
+    //             }
 
-                let indent = 0;
-                while (indent < line.length && line.charAt(indent) === ' ') {
-                    indent++;
-                }
+    //             let indent = 0;
+    //             while (indent < line.length && line.charAt(indent) === ' ') {
+    //                 indent++;
+    //             }
 
-                const nextTrimmedLine = (reversedLines[i - 1] || '').trim();
+    //             const nextTrimmedLine = (reversedLines[i - 1] || '').trim();
 
-                if (
-                    trimmedLine === '}' &&
-                    !/^(else|catch)([^a-zA-Z0-9_]|$)/.test(nextTrimmedLine)
-                ) {
-                    line += ';';
-                }
+    //             if (
+    //                 trimmedLine === '}' &&
+    //                 !/^(else|catch)([^a-zA-Z0-9_]|$)/.test(nextTrimmedLine)
+    //             ) {
+    //                 line += ';';
+    //             }
 
-                nextIndent = indent;
-                return line;
-            })
-            .reverse()
-            .join('\n');
-    }
+    //             nextIndent = indent;
+    //             return line;
+    //         })
+    //         .reverse()
+    //         .join('\n');
+    // }
 
     return code;
 }
