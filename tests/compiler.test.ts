7c3052257bb68d929a2fe5a78d9625272293cdbf
rvanasa <ryan.vandersmith@dfinity.org>
2024-07-09 14:04:38 -0600

    Handle formatting errors from files in compiler test suite

--- tests/compiler.test.ts
+++ tests/compiler.test.ts
@@ -6,22 +6,30 @@ import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
 
 describe('Motoko compiler suite', () => {
     test('generate diff files from compiler tests', async () => {
+        const okFiles: string[] = [];
         for (const extension of ['mo', 'did']) {
             let preOutput = '';
             let postOutput = '';
             for (const file of glob.sync(
                 join(__dirname, `../../motoko/test/**/*.${extension}`),
             )) {
-                const code = readFileSync(file, 'utf-8');
-                const formatted = await prettier.format(code, {
-                    filepath: file,
-                    plugins: [motokoPlugin],
-                });
-                preOutput += `// >>> ${basename(file)} <<<\n\n${code}\n\n`;
-                postOutput += `// >>> ${basename(
-                    file,
-                )} <<<\n\n${formatted}\n\n`;
+                try {
+                    const code = readFileSync(file, 'utf-8');
+                    const formatted = await prettier.format(code, {
+                        filepath: file,
+                        plugins: [motokoPlugin],
+                    });
+                    preOutput += `// >>> ${basename(file)} <<<\n\n${code}\n\n`;
+                    postOutput += `// >>> ${basename(
+                        file,
+                    )} <<<\n\n${formatted}\n\n`;
+                    okFiles.push(file);
+                } catch (err) {
+                    // console.error(`Error while formatting test file: ${file}`);
+                }
             }
+            expect(okFiles.length).toBeGreaterThan(600);
+
             const generatedDir = join(__dirname, 'generated');
             if (!existsSync(generatedDir)) {
                 mkdirSync(generatedDir);
