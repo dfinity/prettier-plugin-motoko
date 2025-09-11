66510e53a3664cd86c0148d2cd56f8b7114a06e3
rvanasa <ryan.vandersmith@dfinity.org>
2025-09-11 12:19:54 -0600

    Add extra newline after import section

--- src/printers/motoko-tt-ast/print.ts
+++ src/printers/motoko-tt-ast/print.ts
@@ -273,6 +273,7 @@ function printTokenTree(
         };
 
         let hasWithKeyword = false;
+        let importSection: boolean | undefined;
         for (let i = 0; i < trees.length; i++) {
             let a = trees[i]!;
 
@@ -296,6 +297,16 @@ function printTokenTree(
                     token.data == 'with'
                 ) {
                     hasWithKeyword = true;
+                } else if (
+                    token.token_type === 'Ident' &&
+                    token.data === 'import'
+                ) {
+                    if (importSection === undefined) {
+                        importSection = true;
+                    }
+                } else if (resultGroup.length === 0 && importSection) {
+                    importSection = false;
+                    resultGroup.push(hardline); /// ???
                 }
             } else if (a.token_tree_type === 'Group') {
                 const [, groupType] = a.data;
